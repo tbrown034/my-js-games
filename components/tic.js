3e6357584fad0dbb99873b67bb66cd55ed97b58e
tbrown034 <tbrown034@gmail.com>
2023-11-16 16:55:11 -0500

    WIP: building out tic logic, also now it hides selectContainer when game is picked

---
+++ components/tic.js
@@ -0,0 +1,25 @@
+//selectors
+
+const ticTurn = document.getElementById("tic-turn");
+const ticBoard = document.getElementById("tic-board");
+const ticWinner = document.getElementById("tic-winner");
+const ticImage = document.getElementById("tic-image");
+const playTic = document.getElementById("play-tic");
+
+// functions
+
+const startGame = () => {
+  // Hide ticImage, show Name
+};
+
+const createBoard = () => {
+  // Hide ticImage, show Name
+};
+
+// helper functions
+
+// event listerns
+
+playTic.addEventListener("click", startGame);
+
+//initializers
--- script.js
+++ script.js
@@ -13,23 +13,36 @@ const rockSection = document.getElementById("rock-section");
 
 const playButton = document.getElementsByClassName("play");
 const backButton = document.querySelectorAll(".back");
+const selectContainer = document.getElementById("select-container");
 
 // helper functions
 
 function showTic() {
   ticSection.style.display = "flex";
+  selectContainer.style.display = "none";
 }
 
 function showConnect() {
   connectSection.style.display = "flex";
+  selectContainer.style.display = "none";
 }
 function showRock() {
   rockSection.style.display = "flex";
+  selectContainer.style.display = "none";
 }
 
 function goBack(e) {
   // This will find the closest parent with the class 'game-section' and hide it
   e.target.closest(".game-section").style.display = "none";
+}
+
+function darkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  const isDarkMode = document.body.classList.contains("dark-mode");
+  darkModeButton.innerHTML = isDarkMode
+    ? '<i class="fa-solid fa-sun"></i> Light Mode'
+    : '<i class="fa-solid fa-circle-half-stroke"></i> Dark Mode';
 }
 
 // event listeners
@@ -41,3 +54,5 @@ selectRock.addEventListener("click", showRock);
 backButton.forEach((backButton) => {
   backButton.addEventListener("click", goBack);
 });
+
+darkModeButton.addEventListener("click", darkMode);
